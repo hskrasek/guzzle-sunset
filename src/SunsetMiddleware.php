e9ac28a80ec2b3229827318fc8fe67eabe4bf094
Hunter Skrasek <hunterskrasek@me.com>
2017-09-06 13:59:43 -0500

    Implement the logic to check the Sunset header and log warnings when the header is present.
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- src/SunsetMiddleware.php
+++ src/SunsetMiddleware.php
@@ -1,5 +1,8 @@
 <?php namespace HSkrasek\Sunset;
 
+use DateTime;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 class SunsetMiddleware
@@ -12,5 +15,55 @@ class SunsetMiddleware
     public function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
+    }
+
+    /**
+     * @param callable $handler
+     *
+     * @return callable
+     */
+    public function __invoke(callable $handler): callable
+    {
+        return function (RequestInterface $request, array $options) use ($handler) {
+            return $handler($request, $options)->then(function (ResponseInterface $response) use ($request) {
+                if ($this->responseHasSunsetHeader($response)) {
+                    $deprecationDate = new DateTime($response->getHeader('Sunset')[0]);
+                    $this->reportDeprecation($request, $deprecationDate);
+                }
+
+                return $response;
+            });
+        };
+    }
+
+    /**
+     * Determine if a Sunset header exists on the response.
+     *
+     * @param ResponseInterface $response
+     *
+     * @return bool
+     */
+    private function responseHasSunsetHeader(ResponseInterface $response): bool
+    {
+        return !empty($response->getHeader('Sunset'));
+    }
+
+    /**
+     * Report the deprecation provided by the Sunset header.
+     *
+     * @param RequestInterface $request
+     * @param DateTime $deprecationDate
+     */
+    private function reportDeprecation(RequestInterface $request, DateTime $deprecationDate): void
+    {
+        if ($deprecationDate > new DateTime) {
+            $this->logger->warning(
+                "Endpoint {$request->getUri()} is deprecated for removal on {$deprecationDate->format('c')}"
+            );
+        } else {
+            $this->logger->warning(
+                "Endpoint {$request->getUri()} was deprecated for removal on {$deprecationDate->format('c')} and could be removed AT ANY TIME"
+            );
+        }
     }
 }
